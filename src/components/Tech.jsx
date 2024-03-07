08c57c57653dd3ae2e92052ce39c98f5e75b095c
sahiltapu <sahilmishranayagarh@gmail.com>
2024-03-07 19:18:01 +0530

    change something

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { BallCanvas } from "./canvas";
+import SectionWraper from "../hoc";
+import { technologies } from "../constrants/constrants";
+import { motion } from "framer-motion";
+import { textVariant } from "../utils/motion";
+import { styles } from "../styles";
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <div className="flex flex-col flex-wrap justify-center gap-10">
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} text-center`}>
+          Tech I have worked with
+        </p>
+      </motion.div>
 
-export default Tech
+      <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((technology) => (
+        <div className="w-28 h-28" key={technology.name}>
+          <BallCanvas icon={technology.icon}/>
+        </div>
+      ))}
+      </div>
+    </div>
+  );
+};
+
+export default SectionWraper(Tech, "");
--- src/constrants/constrants.js
+++ src/constrants/constrants.js
@@ -1,5 +1,7 @@
 import {
   python,
+  pythonlogo,
+  jindalsteel,
   mobile,
   backend,
   creator,
@@ -26,6 +28,8 @@ import {
   threejs,
   datascience,
   softwaredeveloper,
+  ml,
+  c,
 } from "../assets";
 
 export const navLinks = [
@@ -86,16 +90,8 @@ const technologies = [
     icon: javascript,
   },
   {
-    name: "TypeScript",
-    icon: typescript,
-  },
-  {
     name: "React JS",
     icon: reactjs,
-  },
-  {
-    name: "Redux Toolkit",
-    icon: redux,
   },
   {
     name: "Tailwind CSS",
@@ -118,66 +114,31 @@ const technologies = [
     icon: git,
   },
   {
-    name: "figma",
-    icon: figma,
+    name: "Machine Learning",
+    icon: ml,
   },
   {
-    name: "docker",
-    icon: docker,
+    name:"Python",
+    icon: pythonlogo,
   },
+  {
+    name:"c",
+    icon:c,
+  }
 ];
 
 const experiences = [
   {
     title: "React.js Developer",
-    company_name: "Starbucks",
-    icon: starbucks,
-    iconBg: "#383E56",
-    date: "March 2020 - April 2021",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "React Native Developer",
-    company_name: "Tesla",
-    icon: tesla,
+    company_name: "Jindal Steel and Power",
+    icon: jindalsteel,
     iconBg: "#E6DEDD",
-    date: "Jan 2021 - Feb 2022",
+    date: "June 2023 - August 2023",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
       "Implementing responsive design and ensuring cross-browser compatibility.",
       "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "Web Developer",
-    company_name: "Shopify",
-    icon: shopify,
-    iconBg: "#383E56",
-    date: "Jan 2022 - Jan 2023",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "Full stack Developer",
-    company_name: "Meta",
-    icon: meta,
-    iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Developed a robust and user-friendly JSPL Rake Movement system, using React.js, enabling seamless tracking and management of rakes."
     ],
   },
 ];
