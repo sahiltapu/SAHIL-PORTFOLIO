8259fb351f43d7359a6808b710e9b297203a138b
sahiltapu <sahilmishranayagarh@gmail.com>
2024-03-08 20:49:16 +0530

    added some more sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, StarsCanvas, Tech, Works } from "./components";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, StarsCanvas, Tech, Projects } from "./components";
 
 import "./App.css";
 
@@ -14,7 +14,7 @@ function App() {
         <About />
         <Experience />
         <Tech />
-        <Works />
+        <Projects />
         <Feedbacks />
         <div className="relative z-0">
         <Contact />
